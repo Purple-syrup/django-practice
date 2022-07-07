07046276f6fa291c5e7ee261f554ecc230423ade
Purple-syrup <godhanded0@gmail.com>
2022-07-07 01:52:28 +0000

    t

--- authProject/users/views.py
+++ authProject/users/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'users/home.html')
 
 def register(request):
-    form= UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    if request.method=="post":
+        form= UserCreationForm(request.post)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, account created successfully')
+            return redirect('home')
+    else:
+        form=UserCreationForm()
+    return render(request , 'users/register.html', {'form': form})
